66e52270a7f63c264a3adc8f081a3a596dd4821a
mk2leo <72913711+mk2leo@users.noreply.github.com>
2024-12-18 16:58:30 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Mk2 Ai",
+  description: "GPT和千帆可以使用.",
   appleWebApp: {
-    title: "NextChat",
+    title: "Mk2 Ai",
     statusBarStyle: "default",
   },
 };
